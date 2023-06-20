6b7525b73cc8436fc5af75100d664c433c5fc4cf
takesitaharuka <c0a22096ac@edu.teu.ac.jp>
2023-06-20 14:20:19 +0900

    練習2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -20,6 +20,7 @@ def main():
     y = random.randint(0,HEIGHT)  
     bd_rct = bd_img.get_rect()  
     bd_rct.center = x,y  
+    vx,vy = +5, +5
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -30,7 +31,7 @@ def main():
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
         screen.blit(bd_img,bd_rct)
-
+        bd_rct.move_ip(vx,vy)
         pg.display.update()
         tmr += 1
         clock.tick(10)
